c6824114f392480cc1d2a2f8e54925e17813a82a
erinspace <erin.braswell@gmail.com>
2018-04-23 16:18:46 -0400

    Give methods and settings less EZID specific names
    - Change DOI prefix/namespace settings
    - Rename methods to be DOI specific rather than EZID specific

--- website/preprints/tasks.py
+++ website/preprints/tasks.py
@@ -12,7 +12,7 @@ from framework import sentry
 from website import settings, mails
 from website.util.share import GraphNode, format_contributor, format_subject
 from website.identifiers.tasks import update_ezid_metadata_on_change
-from website.identifiers.utils import request_identifiers_from_ezid, parse_identifiers
+from website.identifiers.utils import request_identifiers, parse_identifiers
 
 logger = logging.getLogger(__name__)
 
@@ -181,10 +181,10 @@ def format_preprint(preprint, share_type, old_subjects=None):
 def get_and_set_preprint_identifiers(preprint_id):
     PreprintService = apps.get_model('osf.PreprintService')
     preprint = PreprintService.load(preprint_id)
-    ezid_response = request_identifiers_from_ezid(preprint)
-    if ezid_response is None:
+    doi_client_response = request_identifiers(preprint)
+    if doi_client_response is None:
         return
-    id_dict = parse_identifiers(ezid_response)
+    id_dict = parse_identifiers(doi_client_response)
     preprint.set_identifier_values(doi=id_dict['doi'], save=True)
 
 
