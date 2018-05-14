b181f293522dcb7ff28e582db0453e635e5472cd
John Tordoff <john@cos.io>
2018-05-14 11:19:22 -0400

    update utils, views and scripts to use identifier clients

--- website/identifiers/tasks.py
+++ website/identifiers/tasks.py
@@ -1,7 +1,7 @@
 from django.apps import apps
 
 from framework.celery_tasks import app as celery_app
-from website.identifiers.utils import get_doi_client, build_doi_metadata
+from website.identifiers.utils import get_doi_client
 
 
 @celery_app.task(ignore_results=True)
@@ -11,5 +11,6 @@ def update_doi_metadata_on_change(target_guid, status):
     if target_object.get_identifier('doi'):
         client = get_doi_client(target_object)
         if client:
-            doi, metadata = build_doi_metadata(target_object)
+            metadata = client.build_metadata(target_object)
+            doi = client.build_doi(target_object)
             client.change_status_identifier(status, doi, metadata)
