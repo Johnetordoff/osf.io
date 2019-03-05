6210aea0721d11c7974b5617fb825cefa5278b2a
John Tordoff <john@cos.io>
2019-03-05 17:44:05 -0500

    bug fix for testmon

--- osf_tests/test_node.py
+++ osf_tests/test_node.py
@@ -891,7 +891,7 @@ class TestContributorMethods:
             node.add_contributor(unregistered_user, auth=Auth(user))
         assert excinfo.value.message == 'This contributor cannot be added. ' \
                                         'If the problem persists please report it to please report it to' \
-                                        ' <a href="mailto:support@osf.io">support@osf.io</a>.'
+                                        ' <a href="mailto:{0}">{0}</a>.'.format(settings.OSF_SUPPORT_EMAIL)
 
     def test_cant_add_creator_as_contributor_twice(self, node, user):
         node.add_contributor(contributor=user)
