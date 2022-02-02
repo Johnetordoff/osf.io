2da354e5ca4d36f5f209b7c846da933a13831587
John Tordoff <>
2022-02-02 08:23:44 -0500

    remove profiling tools

--- api/base/settings/local-dist.py
+++ api/base/settings/local-dist.py
@@ -11,24 +11,6 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Uncomment to get real tracebacks while testing
 # DEBUG_PROPAGATE_EXCEPTIONS = True
 
-if DEBUG:
-    INSTALLED_APPS += ('debug_toolbar', 'nplusone.ext.django',)
-    MIDDLEWARE += (
-        'debug_toolbar.middleware.DebugToolbarMiddleware',
-        'nplusone.ext.django.NPlusOneMiddleware',
-    )
-    DEBUG_TOOLBAR_CONFIG = {
-        'SHOW_TOOLBAR_CALLBACK': lambda _: True,
-    }
-    ALLOWED_HOSTS.append('localhost')
-
-    # django-silk
-    INSTALLED_APPS += ('silk',)
-    MIDDLEWARE += (
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'silk.middleware.SilkyMiddleware',
-    )
-
 
 REST_FRAMEWORK['DEFAULT_THROTTLE_RATES'] = {
     'user': '1000000/second',
