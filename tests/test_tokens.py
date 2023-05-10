481a2c602cbd39635c2cb184676a9ed2f7f282e9
John Tordoff <>
2023-05-10 13:50:07 -0400

    create test to reproduce errors

--- tests/test_tokens.py
+++ tests/test_tokens.py
@@ -2,6 +2,7 @@ import jwt
 from rest_framework import status as http_status
 
 import mock
+from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
 from nose.tools import *  # noqa
 
@@ -12,9 +13,19 @@ from tests.utils import mock_auth
 from framework.exceptions import HTTPError
 
 from website import settings
-from osf.models import AbstractNode, Embargo, RegistrationApproval, Retraction, Sanction
+from osf.models import (
+    AbstractNode,
+    Embargo,
+    RegistrationApproval,
+    Retraction,
+    Sanction,
+    Registration
+)
 from osf.utils.tokens import decode, encode, TokenHandler
 from osf.exceptions import TokenHandlerNotFound
+from scripts.embargo_registrations import main as embargo_registrations
+from scripts.approve_registrations import main as approve_registrations
+from django.utils import timezone
 
 NO_SANCTION_MSG = 'There is no {0} associated with this token.'
 APPROVED_MSG = 'This registration is not pending {0}.'
@@ -153,3 +164,52 @@ class TestRetractionTokenHandler(SanctionTokenHandlerBase):
     kind = 'retraction'
     Model = Retraction
     Factory = factories.RetractionFactory
+
+
+class TestEmbargoModerationWorkflow(OsfTestCase):
+
+    kind = None
+    model = None
+    factory = None
+
+    def test_embargo_workflow(self):
+        embargo = factories.EmbargoFactory(end_date=timezone.now())
+        registration = Registration.objects.get(Q(**{'embargo': embargo}))
+        registration_approval = factories.RegistrationApprovalFactory(
+            target_item=registration,
+            initiated_by=registration.creator
+        )
+        assert registration_approval.state == RegistrationApproval.UNAPPROVED
+        assert embargo.state == Embargo.UNAPPROVED
+
+        approval_token = embargo.approval_state[registration.creator._id]['approval_token']
+        handler = TokenHandler.from_string(approval_token)
+
+        with mock_auth(registration.creator):
+            with mock.patch(f'osf.utils.tokens.handlers.embargo_handler') as mock_handler:
+                handler.to_response()
+                mock_handler.assert_called_with('approve', registration, registration.registered_from)
+
+        embargo.reload()
+        assert embargo.state == Embargo.APPROVED
+        assert registration_approval.state == RegistrationApproval.UNAPPROVED
+        registration_approval.initiation_date = timezone.now() - settings.REGISTRATION_APPROVAL_TIME
+        registration_approval.save()
+
+        approve_registrations(dry_run=False)
+
+        registration_approval.reload()
+        assert registration_approval.state == RegistrationApproval.APPROVED
+
+        registration.reload()
+        assert registration.embargo
+        assert registration.embargo.state == Embargo.APPROVED
+        assert not registration.is_public
+
+        embargo_registrations(dry_run=False)
+
+        registration.embargo.reload()
+        assert registration.embargo.state == Embargo.COMPLETED
+
+        registration.reload()
+        assert registration.is_public
